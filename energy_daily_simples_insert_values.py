7bbb389b761f5b2ef217b8f090dd0b19bdfa9c89
root <root@a0d7b954-vscode.local.hass.io>
2020-04-01 15:05:30 +0100

    Insert script now creates the json file if it doesn't exist

--- energy_daily_simples_insert_values.py
+++ energy_daily_simples_insert_values.py
@@ -3,11 +3,15 @@
 import json
 import datetime
 import sys
+import os
 
 def main(argv):
   dt = datetime.datetime.today()
   json_file_path = '/config/data/energy_daily_simples_kw.json'
   
+  if not os.path.exists(json_file_path):
+    with open(json_file_path, 'w'): pass
+
   with open(json_file_path, 'r') as data_file:
     try:
       data = json.load(data_file)
